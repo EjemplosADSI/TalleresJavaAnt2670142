964e546fc4be135328c6999082b51b40cfa718d3
Diego Alonso Ojeda Medina <daom89@gmail.com>
2023-09-07 16:33:16 -0500

    Se agregaron reglas de validacion de numeros

--- src/talleresjavaant2670142/frmE01.java
+++ src/talleresjavaant2670142/frmE01.java
@@ -5,7 +5,19 @@
 package talleresjavaant2670142;
 
 import java.text.DecimalFormat;
-import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/*
+En este problema, los datos de entrada son los dos valores enteros que ingresará 
+el usuario a través del teclado (los llamaremos a y b) y la salida será su 
+cociente (un número flotante). Ahora bien, existe la posibilidad de que el 
+usuario ingrese como segundo valor el número 0 (cero). En este caso, no podremos 
+mostrar el cociente ya que la división por cero es una indeterminación, así que 
+tendremos que emitir un mensaje informando las causas por las cuales no se podrá 
+efectuar la operación.
+
+*/
+
 
 /**
  *
@@ -204,18 +216,37 @@ public class frmE01 extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_exitMenuItemActionPerformed
 
+    public boolean isNumber (String numero){
+        try{
+            int Number = Integer.parseInt(numero);
+            return true;
+        }catch(NumberFormatException NFE){
+            JOptionPane.showMessageDialog(this, 
+                    "El texto "+numero +" no es un numero valido", 
+                    "Numero Invalido", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+    
     private void btnCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalcularActionPerformed
         // TODO add your handling code here:
         int N1 = 0, N2 = 0;
         float Resultado = 0.0f;
         DecimalFormat df = new DecimalFormat("0.00");
         
-        N1 = Integer.parseInt(txtNumero1.getText());
-        N2 = Integer.parseInt(txtNumero2.getText());
-        Resultado = (float) N1 / N2;
-        
-        lblResultado.setText(df.format(Resultado));
-        lblResultado.setVisible(true);
+        if(txtNumero2.getText().equalsIgnoreCase("0")){
+            JOptionPane.showMessageDialog(this, "Error 0 no esta permitido como segundo numero", "Valor Incorrecto", JOptionPane.ERROR_MESSAGE);
+            txtNumero2.setText("");
+        }else{
+            if(isNumber(txtNumero1.getText()) && isNumber(txtNumero2.getText())){
+                N1 = Integer.parseInt(txtNumero1.getText());
+                N2 = Integer.parseInt(txtNumero2.getText());
+                Resultado = (float) N1 / N2;
+
+                lblResultado.setText(df.format(Resultado));
+                lblResultado.setVisible(true);
+            }
+        }
     }//GEN-LAST:event_btnCalcularActionPerformed
 
     /**
